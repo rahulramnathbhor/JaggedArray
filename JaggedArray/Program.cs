13e912ca7395c395e347cc7ac665b8ae5f7da18a
rahulramnathbhor <112869809+rahulramnathbhor@users.noreply.github.com>
2022-09-21 15:55:26 +0530

    Add project files.

---
+++ JaggedArray/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JaggedArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int[][] jaggeArray = new int[4][];
+
+            jaggeArray[0] = new int[] { 1, 2, 3, 4, 5 }; 
+            jaggeArray[1] = new int[] { 1, 2, 3 };
+            jaggeArray[2] = new int[] { 1, 2 };
+            jaggeArray[3]= new int[] { 1, 2 ,5,6,7,8};
+
+            for (int i = 0; i < jaggeArray.Length; i++)
+            {
+                for (int j = 0; j < jaggeArray[i].Length; j++)
+                {
+                    Console.Write(jaggeArray[i][j] + "  ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+    }
+}
